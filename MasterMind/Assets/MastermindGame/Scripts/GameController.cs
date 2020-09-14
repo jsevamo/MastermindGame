3b25881f3f2d5929dbd94f9630a183dd70e7c029
jsevamo <j_sevamo@hotmail.com>
2020-09-14 11:27:29 -0500

    Board Piece Spawns on (0,0,0)

---
+++ MasterMind/Assets/MastermindGame/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace MastermindGame.Scripts
+{
+    public class GameController : MonoBehaviour
+    {
+
+        [SerializeField] [Range(2.0f, 4.0f)] private int numberToGuess = 4;
+        [SerializeField] private GameObject boardPiece;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            Instantiate(boardPiece, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
